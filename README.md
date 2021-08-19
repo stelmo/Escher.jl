a4f5096503ad370221528570e10b95afe4d080bf
St. Elmo <stelmozors@gmail.com>
2021-08-19 11:17:23 +0200

    format + attributes fix

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Escher.jl
- Plot maps of metabolic models
+
+This package implements a single [Makie](https://makie.juliaplots.org/stable/index.html)
+recipe called `escherplot` that plots maps of metabolic models resembling its [namesake
+GUI](https://escher.github.io/#/). It's primary purpose is to facilitate the generation of
+high quality metabolic maps from within Julia.
+
+## Example
+```
+using Escher
+using CairoMakie
+
+escherplot("maps/core-map.json")
+```
+
+## Attributes
+The `escherplot` recipe exposes a number of custom attributes that can be used to modify the
+basic metabolic map figure.
--- src/Escher.jl
+++ src/Escher.jl
@@ -2,16 +2,20 @@ module Escher
 
 using JSON, Makie
 
-@recipe(EscherPlot) do scene
-    Attributes(
-        primary_node_size = 5,
-        secondary_node_size = 3,
-    )
-end
+_bezier(t, p0, p1, p2, p3) =
+    (1 - t)^3 .* p0 + 3 * (1 - t)^2 * t .* p1 + 3 * (1 - t) * t^2 .* p2 + t^3 .* p3
 
-function _nodes(escher; primary_node_size = 5, secondary_node_size=3)
+function _nodes(
+    escher;
+    primary_node_size = 5,
+    secondary_node_size = 3,
+    metabolite_identifier = "bigg_id",
+)
     nodexs = Float64[]
     nodeys = Float64[]
+    labelxs = Float64[]
+    labelys = Float64[]
+    metabolite_labels = String[]
     node_pos = Dict()
     markersizes = Float64[]
     for (node_id, node) in escher["nodes"]
@@ -21,6 +25,11 @@ function _nodes(escher; primary_node_size = 5, secondary_node_size=3)
             node_pos[node_id] = (x, y)
             # plotted nodes
             if node["node_type"] == "metabolite"
+                if haskey(node, metabolite_identifier)
+                    push!(metabolite_labels, node[metabolite_identifier])
+                    push!(labelxs, node["label_x"])
+                    push!(labelys, -node["label_y"])
+                end
                 push!(nodexs, x)
                 push!(nodeys, y)
                 if node["node_is_primary"]
@@ -31,35 +40,107 @@ function _nodes(escher; primary_node_size = 5, secondary_node_size=3)
             end
         end
     end
-    return nodexs, nodeys, node_pos, markersizes
+    return nodexs, nodeys, node_pos, markersizes, labelxs, labelys, metabolite_labels
 end
 
-function Makie.plot!(ep::EscherPlot{<:Tuple{String, Dict{String, Float64}, String}})
+@recipe(EscherPlot) do scene
+    Attributes(
+        metabolite_show_text = false,
+        metabolite_text_size = 4,
+        metabolite_primary_node_size = 5,
+        metabolite_secondary_node_size = 3,
+        metabolite_node_color = :black,
+        metabolite_text_color = :black,
+        metabolite_identifier = "bigg_id",
+        reaction_show_text = false,
+        reaction_text_size = 4,
+        reaction_identifier = "bigg_id",
+        reaction_edge_width = 2.0,
+        reaction_edge_color_weights = Dict{String,Float64}(),
+        reaction_edge_width_weights = Dict{String,Float64}(),
+    )
+end
+
+function Makie.plot!(ep::EscherPlot{<:Tuple{String}})
 
     _, escher = JSON.parsefile(to_value(ep[1]))
-    fluxes_in = ep[2]
-    reaction_identifier = to_value(ep[3])
 
-    fluxes = Node(Float64[])
-    rids_in_map = [rxn[reaction_identifier] for rxn in values(escher["reactions"]) if haskey(rxn, reaction_identifier)]
+    reaction_identifier = to_value(ep.reaction_identifier)
 
-    function update_plot(fluxes_in)
-        empty!(fluxes[])
-        for rid in rids_in_map # update in order
-            if haskey(to_value(fluxes_in), rid)
-                push!(fluxes[], abs(to_value(fluxes_in[rid])))
+    metabolite_node_xs,
+    metabolite_node_ys,
+    metabolite_node_pos,
+    metabolite_markersizes,
+    metabolite_label_xs,
+    metabolite_label_ys,
+    metabolite_labels = _nodes(
+        escher;
+        primary_node_size = to_value(ep.metabolite_primary_node_size),
+        secondary_node_size = to_value(ep.metabolite_secondary_node_size),
+    )
+
+    lw = 0.0
+    for (rid, rxn) in escher["reactions"]
+        for (_, segment) in rxn["segments"]
+            if !isnothing(segment["b1"]) && !isnothing(segment["b2"])
+                n1 = metabolite_node_pos[segment["from_node_id"]]
+                p0 = [n1[1], n1[2]]
+                p1 = [segment["b1"]["x"], -segment["b1"]["y"]]
+                p2 = [segment["b2"]["x"], -segment["b2"]["y"]]
+                n2 = metabolite_node_pos[segment["to_node_id"]]
+                p3 = [n2[1], n2[2]]
+                ts = range(0.0, 1.0; length = 10)
+                ds = _bezier.(ts, Ref(p0), Ref(p1), Ref(p2), Ref(p3))
+                xs = [d[1] for d in ds]
+                ys = [d[2] for d in ds]
+                if lw == -1.0
+                    lines!(ep, xs, ys, linewidth = ep.reaction_edge_width, linestyle = :dot)
+                else
+                    lines!(ep, xs, ys, linewidth = ep.reaction_edge_width)
+                end
             else
-                push!(fluxes[], -1.0)
+                n1 = metabolite_node_pos[segment["from_node_id"]]
+                n2 = metabolite_node_pos[segment["to_node_id"]]
+                if lw == -1.0
+                    lines!(
+                        ep,
+                        [n1[1], n2[1]],
+                        [n1[2], n2[2]],
+                        linewidth = ep.reaction_edge_width,
+                        linestyle = :dot,
+                    )
+                else
+                    lines!(
+                        ep,
+                        [n1[1], n2[1]],
+                        [n1[2], n2[2]],
+                        linewidth = ep.reaction_edge_width,
+                    )
+                end
             end
         end
     end
 
-    Makie.Observables.onany(update_plot, fluxes_in)
-    update_plot(fluxes_in)
+    scatter!(
+        ep,
+        metabolite_node_xs,
+        metabolite_node_ys,
+        color = ep.metabolite_node_color,
+        markersize = metabolite_markersizes,
+    )
 
-    nodexs, nodeys, node_pos, markersizes = _nodes(escher; primary_node_size = to_value(ep.primary_node_size), secondary_node_size=to_value(ep.secondary_node_size))
-
-    scatter!(ep, nodexs, nodeys, color=:black, markersize=markersizes)
+    if ep.metabolite_show_text[]
+        positions = [
+            (metabolite_label_xs[i], metabolite_label_ys[i]) for
+            i = 1:length(metabolite_labels)
+        ]
+        text!(
+            ep,
+            metabolite_labels;
+            position = positions,
+            textsize = ep.metabolite_text_size,
+        )
+    end
 
     ep
 end
@@ -79,7 +160,6 @@ export plot!
 #     end
 
 #     # Bezier cubic
-#     B3(t, p0, p1, p2, p3) = (1-t)^3 .* p0  + 3 * (1-t)^2 * t .* p1 + 3 * (1-t) * t^2 .* p2 + t^3 .* p3
 #     fallback_lw = 2.0
 #     for (rid, rxn) in escher["reactions"]
 
